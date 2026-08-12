c687c4539917fb0e6123cbf7a8a805accb6ebb32
juanpabloma2005-stack <juanpabloma2005@gmail.com>
2026-08-12 00:55:17 -0600

    wip: primer intento de aplicarIVA, sigue en rojo por punto flotante

--- src/carrito.js
+++ src/carrito.js
@@ -2,4 +2,8 @@ function calcularSubtotal(items) {
   return items.reduce((total, item) => total + item.precio * item.cantidad, 0);
 }
 
-module.exports = { calcularSubtotal };
+function aplicarIVA(monto) {
+  return monto * 1.13;
+}
+
+module.exports = { calcularSubtotal, aplicarIVA };
