ac5d3ae61ffd88a8e265692b806b6b123538ff22
juanpabloma2005-stack <juanpabloma2005@gmail.com>
2026-08-12 01:00:47 -0600

    fix(GREEN): corregir con helper de redondeo

--- src/carrito.js
+++ src/carrito.js
@@ -1,9 +1,15 @@
+//Prueba 1
 function calcularSubtotal(items) {
   return items.reduce((total, item) => total + item.precio * item.cantidad, 0);
 }
 
+//Prueba 2
+function redondear(monto) {
+  return Math.round(monto * 100) / 100;
+}
+
 function aplicarIVA(monto) {
-  return monto * 1.13;
+  return redondear(monto * 1.13);
 }
 
 module.exports = { calcularSubtotal, aplicarIVA };
